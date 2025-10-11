a1e5336d11d213a53c3d3347b63ff02efa0f1e45
Joseph Johnson <jwjbadger@gmail.com>
2025-10-11 02:10:42 -0500

    [BUILD FAILS] Add server configuration for services and characteristics

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use esp_idf_hal::{delay::FreeRtos, gpio, prelude::*};
-use otgi::{obd, wireless};
-use std::sync::Arc;
 use esp_idf_svc::{
     bt::{
-        Ble, BtDriver,
         ble::{gap::EspBleGap, gatt::server::EspGatts},
+        Ble, BtDriver, BtUuid,
     },
     nvs::{self, EspDefaultNvsPartition},
 };
+use otgi::{obd, wireless};
+use std::sync::Arc;
+
+const SERVICE_UUID: u128 = 0x2cbc6002370f577a928681e04f368400;
+const IND_CHARACTERISTIC_UUID: u128 = 0x56c46fef90390803a71feebcc8650e43;
 
 fn main() {
     esp_idf_svc::sys::link_patches();
@@ -16,7 +19,8 @@ fn main() {
     let peripherals = Peripherals::take().unwrap();
 
     let nvs_partition = EspDefaultNvsPartition::take().unwrap();
-    let bt = Arc::new(BtDriver::<Ble>::new(peripherals.modem, Some(nvs_partition.clone())).unwrap());
+    let bt =
+        Arc::new(BtDriver::<Ble>::new(peripherals.modem, Some(nvs_partition.clone())).unwrap());
     let pins = peripherals.pins;
 
     // Commenting this out until absolutely necessary to reduce the write count to nvs
@@ -40,6 +44,25 @@ fn main() {
     let ble_server = wireless::Server::new(
         Arc::new(EspBleGap::new(bt.clone()).unwrap()),
         Arc::new(EspGatts::new(bt.clone()).unwrap()),
+        wireless::ServerConfiguration {
+            services: vec![
+                wireless::ServiceDescriptor {
+                    uuid: BtUuid::uuid128(SERVICE_UUID),
+                    is_primary: true
+                }
+            ],
+            characteristics: vec![
+                wireless::CharacteristicDescriptor {
+                    uuid: BtUuid::uuid128(IND_CHARACTERISTIC_UUID),
+                    service_uuid: BtUuid::uuid128(SERVICE_UUID),
+                    permissions: (Permission::Write | Permission::Read).into(),
+                    properties: (Property::Indicate).into(),
+                    max_len: 200,
+                    data: liters_used.to_le_bytes()
+                }
+            ],
+            name: "OTGI",
+        },
     );
 
     let gap_server = ble_server.clone();
@@ -65,7 +88,13 @@ fn main() {
     let mut high_ref = gpio::PinDriver::output(pins.gpio25).unwrap();
     high_ref.set_high().unwrap();
 
-    let mut driver = obd::ObdDriver::try_new(peripherals.can, pins.gpio33, pins.gpio32, &Default::default()).unwrap();
+    let mut driver = obd::ObdDriver::try_new(
+        peripherals.can,
+        pins.gpio33,
+        pins.gpio32,
+        &Default::default(),
+    )
+    .unwrap();
     driver.start().expect("Failed to start driver");
 
     let mut liters_used: f32 = 0.0;
@@ -81,10 +110,15 @@ fn main() {
         if let (Ok(maf), Ok(stft), Ok(ltft)) = (maf, stft, ltft) {
             let usage = (maf * 3600.0) / ((14.7 * (1.0 + ((stft + ltft) / 100.0))) * 740.0);
             liters_used += (usage / 3600.0) * 0.3; // rough approximation (300 ms)
-            log::info!("Estimated fuel usage (L/h): {:?}; {:?} liters used in total", usage, liters_used);
+            log::info!(
+                "Estimated fuel usage (L/h): {:?}; {:?} liters used in total",
+                usage,
+                liters_used
+            );
         }
 
         // UNTESTED
+        liters_used += 0.1;
         ble_server.indicate(&liters_used.to_le_bytes()).unwrap();
     }
 }
--- src/wireless.rs
+++ src/wireless.rs
@@ -1,7 +1,8 @@
+use enumset::EnumSet;
 use esp_idf_svc::{
     bt::{
         ble::{
-            gap::{AdvConfiguration, BleGapEvent, EspBleGap},
+            gap::{AdvConfiguration, AppearanceCategory, BleGapEvent, EspBleGap},
             gatt::{
                 server::{EspGatts, GattsEvent},
                 AutoResponse, GattCharacteristic, GattDescriptor, GattId, GattInterface,
@@ -17,8 +18,6 @@ use std::sync::{Arc, Condvar, Mutex};
 
 // TODO: Determine proper IDs
 pub const APP_ID: u16 = 0;
-pub const SERVICE_UUID: u128 = 0x2cbc6002370f577a928681e04f368400;
-pub const IND_CHARACTERISTIC_UUID: u128 = 0x56c46fef90390803a71feebcc8650e43;
 pub const MAX_CONNECTIONS: usize = 1;
 
 #[derive(Clone)]
@@ -27,6 +26,7 @@ pub struct Server {
     pub gatts: Arc<EspGatts<'static, Ble, Arc<BtDriver<'static, Ble>>>>,
     state: Arc<Mutex<State>>,
     cvar: Arc<Condvar>,
+    config: ServerConfiguration,
 }
 
 #[derive(Debug, Clone)]
@@ -44,16 +44,53 @@ struct State {
     service_handle: Option<Handle>,
 }
 
+#[derive(Clone)]
+pub struct ServiceDescriptor {
+    pub uuid: BtUuid, // I see no usage for the instance id so it will be hardcoded 0 unless I
+    // learn what its purpose is
+    pub is_primary: bool, // This will not be verified; the system will likely panic if you make two
+                          // things primary, and that is perfectly acceptable
+}
+
+#[derive(Clone)]
+pub struct CharacteristicDescriptor {
+    pub uuid: BtUuid,
+    pub service_uuid: BtUuid,
+    pub permissions: EnumSet<Permission>,
+    pub properties: EnumSet<Property>,
+    pub max_len: usize, // number of bytes
+    pub data: Vec<u8>,
+}
+
+#[derive(Clone)]
+pub struct ServerConfiguration {
+    pub services: Vec<ServiceDescriptor>,
+    pub characteristics: Vec<CharacteristicDescriptor>,
+    pub name: &'static str,
+}
+
+impl Default for ServerConfiguration {
+    fn default() -> Self {
+        Self {
+            services: vec![], // As of now, only one service should be created
+            characteristics: vec![],
+            name: "esp32",
+        }
+    }
+}
+
 impl Server {
     pub fn new(
         gap: Arc<EspBleGap<'static, Ble, Arc<BtDriver<'static, Ble>>>>,
         gatts: Arc<EspGatts<'static, Ble, Arc<BtDriver<'static, Ble>>>>,
+        config: ServerConfiguration,
     ) -> Self {
         Self {
             gap,
             gatts,
             state: Default::default(),
             cvar: Arc::new(Condvar::new()),
+            config,
         }
     }
 
@@ -82,29 +119,37 @@ impl Server {
                 }
 
                 if app_id == APP_ID {
-                    self.gap.set_device_name("OTGI").unwrap();
+                    self.gap.set_device_name(self.config.name).unwrap();
                     self.gap
                         .set_adv_conf(&AdvConfiguration {
                             include_name: true,
-                            include_txpower: true,
+                            include_txpower: false,
+                            // Since we interface with a car that interfaces with a human
+                            // Or generally everything indirectly interfaces with a human
+                            appearance: AppearanceCategory::HumanInterfaceDevice,
                             flag: 2,
-                            service_uuid: Some(BtUuid::uuid128(SERVICE_UUID)),
+                            service_uuid: Some(self.config.services.iter().filter(|s| s.is_primary == true).next().expect("No primary service").uuid), // TODO: primary
+                            // service
                             ..Default::default()
                         })
                         .unwrap();
-                    self.gatts
-                        .create_service(
-                            gatt_intf,
-                            &GattServiceId {
-                                id: GattId {
-                                    inst_id: 0,
-                                    uuid: BtUuid::uuid128(SERVICE_UUID),
+
+                    // TODO: avoid cloning by draining the config as necessary
+                    self.config.services.iter().for_each(|service_descriptor| {
+                        self.gatts
+                            .create_service(
+                                gatt_intf,
+                                &GattServiceId {
+                                    id: GattId {
+                                        inst_id: 0,
+                                        uuid: service_descriptor.uuid.clone(),
+                                    },
+                                    is_primary: service_descriptor.is_primary,
                                 },
-                                is_primary: true,
-                            },
-                            8,
-                        )
-                        .unwrap();
+                                8, // TODO: automatically calculate num_handles
+                            )
+                            .unwrap();
+                    });
 
                     self.state.lock().unwrap().gatt_intf = Some(gatt_intf);
                 }
@@ -112,7 +157,7 @@ impl Server {
             GattsEvent::ServiceCreated {
                 status,
                 service_handle,
-                ..
+                service_id,
             } => {
                 if status != GattStatus::Ok {
                     panic!("Failed to create GATT service: {:?}", status);
@@ -121,19 +166,26 @@ impl Server {
                 self.state.lock().unwrap().service_handle = Some(service_handle);
                 self.gatts.start_service(service_handle).unwrap();
 
-                self.gatts
-                    .add_characteristic(
-                        service_handle,
-                        &GattCharacteristic {
-                            uuid: BtUuid::uuid128(IND_CHARACTERISTIC_UUID),
-                            permissions: (Permission::Write | Permission::Read).into(),
-                            properties: (Property::Indicate).into(),
-                            max_len: 200,
-                            auto_rsp: AutoResponse::ByApp,
-                        },
-                        &[],
-                    )
-                    .unwrap();
+                self.config
+                    .characteristics
+                    .iter()
+                    .filter(|char| char.service_uuid == service_id.id.uuid)
+                    .for_each(|char| {
+                        self.gatts
+                            .add_characteristic(
+                                service_handle,
+                                &GattCharacteristic {
+                                    uuid: char.uuid.clone(),
+                                    permissions: char.permissions,
+                                    properties: char.properties,
+                                    max_len: char.max_len,
+                                    auto_rsp: AutoResponse::ByApp, // I see no forseeable reason to ever
+                                                                   // change this
+                                },
+                                char.data.as_slice(),
+                            )
+                            .unwrap();
+                    });
             }
             GattsEvent::PeerConnected { conn_id, addr, .. } => {
                 let mut state = self.state.lock().unwrap();
